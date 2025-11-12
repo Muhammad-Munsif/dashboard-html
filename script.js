705ba640239d981a2f98ae6217425da836c4d9a8
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-12 22:26:16 +0500

    few changes in html modal html and css and Js file

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Sidebar Toggle Function
 function toggleSidebar() {
-    let sidebar = document.getElementById("sidebar");
-    if (sidebar.style.width === "250px") {
-        sidebar.style.width = "50px";
-    } else {
-        sidebar.style.width = "250px";
-    }
+  let sidebar = document.getElementById("sidebar");
+  if (sidebar.style.width === "250px") {
+    sidebar.style.width = "50px";
+  } else {
+    sidebar.style.width = "250px";
+  }
 }
 
 // Dropdown Menu Toggle
 function toggleDropdown(menuId) {
-    let menu = document.getElementById(menuId);
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
+  let menu = document.getElementById(menuId);
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
 }
