e14c1fc3a35a456f0f37167cb9db6d8b27a83826
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-14 21:41:02 +0500

    small changes catagory and customers file

--- Customers.jsx
+++ Customers.jsx
@@ -1,12 +1,14 @@
 const Customers = () => {
-    return (
-      <div className="pt-24">
-        <div className="bg-white rounded-xl shadow-sm p-6">
-          <h1 className="text-2xl font-bold text-gray-900 mb-6">Customers Management</h1>
-          <p>Customers content goes here...</p>
-        </div>
+  return (
+    <div className="pt-24">
+      <div className="bg-white rounded-xl shadow-sm p-6">
+        <h1 className="text-2xl font-bold text-gray-900 mb-6">
+          Customers Management
+        </h1>
+        <p>Customers content goes here...</p>
       </div>
-    );
-  };
-  
-  export default Customers;
+    </div>
+  );
+};
+
+export default Customers;
