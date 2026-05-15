2e4f9eeb50350b525da9a8f9a957ff8f97df7a1c
Muhammad-Munsif <m.munsif4375@gmail.com>
2026-05-15 23:23:24 +0500

    this is a small changes inside the css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-<script>
+
     (function () {
       let employeesData = [
         { id: "0001", name: "Anika Vaccaro", designation: "Manager", department: "Operations", team: "Super A", supervisor: "Kaylo Margo", tenure: "1 Yr, 2 Mo", profile: 95, shift: "Morning", city: "New York", state: "NY", religion: "Christian", avatar: "https://randomuser.me/api/portraits/women/68.jpg", role: "admin" },
@@ -220,4 +220,4 @@
       app.showSection("dashboard");
       if (window.innerWidth > 768) document.getElementById("mainContainer").style.marginLeft = "280px";
     })();
-  </script>
+  
