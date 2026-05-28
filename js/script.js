48d924395b3e84f6efd0f940462ab08915cd70f2
Muhammad-Munsif <m.munsif4375@gmail.com>
2026-05-28 23:37:45 +0500

    this is a small changes inside the css style to make it great

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-<script>
+
     // ==================== DATA ====================
     let employees = [
       { id: "0001", name: "Anika Vaccaro", designation: "Manager", department: "Operations", team: "Alpha", supervisor: "John Smith", tenure: "2 Yrs", profile: 95, shift: "Morning", city: "New York", avatar: "https://randomuser.me/api/portraits/women/68.jpg", role: "admin" },
@@ -323,4 +323,4 @@
     initTheme();
     renderTables();
     showSection("dashboard");
-  </script>
+  
